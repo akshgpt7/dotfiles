ce3251dfed5a8ff7e06068074b12ebb639bea779
akshgpt7 <akshgpt7@gmail.com>
2020-10-03 01:37:46 +0530

    add fish and starship configs

---
+++ fish/config.fish
@@ -0,0 +1,19 @@
+set -g -x fish_greeting ''
+
+abbr -a x 'exit'
+abbr -a c 'clear'
+abbr -a nv 'nvim'
+abbr -a la 'ls -A'
+abbr -a l 'ls -CF'
+
+# some git abbreviations
+abbr -a gs 'git status'                                                           
+abbr -a gadd 'git add'                                                            
+abbr -a gc 'git commit -S -m'
+abbr -a gb 'git branch'
+
+abbr -a python 'python3'
+abbr -a pip 'pip3'
+
+starship init fish | source
+
