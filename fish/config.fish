09b32d44490c551923c11023b74dd0876521c229
akshgpt7 <akshgpt7@gmail.com>
2020-10-03 01:47:58 +0530

    remove signed commit flag

--- fish/config.fish
+++ fish/config.fish
@@ -9,7 +9,7 @@ abbr -a l 'ls -CF'
 # some git abbreviations
 abbr -a gs 'git status'                                                           
 abbr -a gadd 'git add'                                                            
-abbr -a gc 'git commit -S -m'
+abbr -a gc 'git commit -m'
 abbr -a gb 'git branch'
 
 abbr -a python 'python3'
